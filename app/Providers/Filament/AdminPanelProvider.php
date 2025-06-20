9b742b95f227cef2a3c3e60037b8068e4cb4101e
Rindi Lenovo LOQ <budiaramdhanrindi@gmail.com>
2025-06-20 19:59:02 +0700

    change color theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,15 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                // 'primary' => Color::Amber,
+                'danger' => Color::Red,
+                'gray' => Color::Gray,
+                'info' => Color::Lime,
+                'primary' => Color::Blue,
+                'success' => Color::Green,
+                'warning' => Color::Orange,
             ])
+            ->darkMode(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
